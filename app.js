61611faa3dca0bc7ad5cc76a39ec341eb2cb0841
kevinlampe1998 <kevin.lampe@dci-student.org>
2024-10-03 14:34:24 +0200

    Welcome greet on root

--- app.js
+++ app.js
@@ -25,6 +25,8 @@ app.use(cors({
 app.use(express.json());
 app.use(cookieParser());
 
+app.get('/', (req, res) => res.send('Hello from ToDoManager Backend'))
+
 app.post('/users-register', async (req, res) => {
 
   const { username, password } = req.body;
