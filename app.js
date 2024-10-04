a74f5490bb2114ac29fbcfa20a9ff7f85ecd7946
kevinlampe1998 <kevin.lampe@dci-student.org>
2024-10-04 08:49:45 +0200

    set secure: true

--- app.js
+++ app.js
@@ -83,7 +83,7 @@ app.post('/users-login', async (req, res) => {
     res.cookie('token', token, {
       // httpOnly: true,
       // maxAge: 3_600_000,
-      // secure: true,
+      secure: true,
       // secure: false,
       sameSite: 'None'
     });
